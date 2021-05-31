799587c5020efd3d833f5db2ccb2743ce0822417
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-05-31 22:37:42 +1200

    Header base done

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Project Initialised using Create React App with Redux Template
 Uses Styled Components
 
 Images found from Clever Programmer
+
+Material UI
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
         <Container>
+            <a><img src="images/logo.svg" /></a>
+            <Menu>
+                <a href="a">Model S</a>
+                <a href="a">Model Y</a>
+                <a href="a">Model 3</a>
+                <a href="a">Model X</a>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <CustomMenu>
 
+                </CustomMenu>
+            </RightMenu>
         </Container>
     )
 }
@@ -12,5 +26,49 @@ function Header() {
 export default Header
 
 const Container = styled.div`
-    
+    min-height: 60px;
+    position: fixed; // important stays on top no zIndex needed
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+
+`
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+        flex-wrap: nowrap;
+    }
+`
+
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
 `
